3dea1026c0ede8f8f4223d1297884fbdc60ebec6
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-11-17 19:12:24 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const container = document.querySelector(".gallery");
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery_item">
+    <a class="gallery_link" href="${original}">
+    <img class="gallery_image" src="${preview}" alt="${description}" />
+    </a>
+    </li>
+    `
+    )
+    .join("");
+}
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
